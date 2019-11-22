243511a84f7f407d036305afb83f744a7f093041
Dtown <dannyotown@gmail.com>
2019-11-22 12:24:49 -0600

    fixed logo bug

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,8 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Nav, Links, Logo, LogoAgain, Navvy } from "../styling/NavStyling";
+import logo from "../styling/logo.png";
+import logo2 from "../styling/logo2.png";
 
 export default function NavBar({ loggedInOrOut }) {
   return (
@@ -11,8 +13,8 @@ export default function NavBar({ loggedInOrOut }) {
         <div>
           <Router>
             <Nav>
-              <Logo src={Logo}></Logo>
-              <LogoAgain src="logo2.png"></LogoAgain>
+              <Logo src={logo}></Logo>
+              <LogoAgain src={logo2}></LogoAgain>
               <Navvy>
                 <Links href="/">Home</Links>
                 {loggedInOrOut && <Links href="/login">Login</Links>}
